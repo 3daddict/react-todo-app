99bf3efc7f95666a92d9d5b308d513f36251dc84
Michael Salvati <print@3daddict.com>
2018-09-19 16:32:05 -0700

    Completed add function

--- src/components/list.js
+++ src/components/list.js
@@ -1,26 +1,7 @@
-import React, {Component} from 'react';
-import dummyListData from '../dummy_data/list_data';
+import React from 'react';
 
-class List extends Component {
-
-    state = {
-        list: []
-    }
-
-    componentDidMount() {
-        this.getListData();
-    }
-
-    getListData() {
-        //Call server to get data
-
-        this.setState({
-            list: dummyListData
-        });
-    }
-
-    render(){
-        const listElements = this.state.list.map((item, index) => {
+const List = (props) => {
+        const listElements = props.data.map((item, index) => {
             return <li className="collection-item" key={item._id}>{item.title}</li>
         });
 
@@ -30,6 +11,5 @@ class List extends Component {
                 </ul>
         )
     }
-}
 
 export default List;
