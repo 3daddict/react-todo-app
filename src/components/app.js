0e0e8e506f4907dbe0c9f17045e68719c6785df7
Michael Salvati <print@3daddict.com>
2018-09-19 15:05:20 -0700

    completed display dummy data

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,11 @@
+import 'materialize-css/dist/css/materialize.min.css'
 import React from 'react';
-import '../assets/css/app.css';
-import logo from '../assets/images/logo.svg';
+import List from './list'
 
 const App = () => (
-    <div>
-        <div className="app">
-            <img src={logo} className="logo rotate"/>
-            <h1>Welcome to React</h1>
-        </div>
+    <div className="container">
+        <h1 className="center">Todo App</h1>
+        <List />
     </div>
 );
 
---
+++ src/dummy_data/list_data.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default [
+    {
+        title: 'Go to store',
+        details: 'Get eggs, milk, bread and diet coke',
+        _id: '001'
+    },
+    {
+        title: 'Go to the Bank',
+        details: 'Withdrawl a ton of money, make sure they are singles',
+        _id: '002'
+    },
+    {
+        title: 'Pickup Dry Cleaning',
+        details: 'Get shirts, pants',
+        _id: '003'
+    },
+    {
+        title: 'Pickup the kid from school',
+        details: 'Drive to school and pickup the kid, make sure he has his backpack this time',
+        _id: '004'
+    },
+    {
+        title: 'Pickup dinner',
+        details: 'Pickup dinner on way home, check order for mistakes.',
+        _id: '005'
+    }
+]
