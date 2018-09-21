c16ec81589b4f7691a8d380654d84fbd66676be8
Michael Salvati <print@3daddict.com>
2018-09-21 13:07:52 -0700

    completed connect to server

--- src/components/list.js
+++ src/components/list.js
@@ -3,7 +3,7 @@ import Item from './item';
 
 const List = (props) => {
         const listElements = props.data.map((item, index) => {
-            return <Item key={item._id} item={item} delete={() => props.delete(index)} />
+            return <Item key={item._id} item={item} delete={() => props.delete(item._id)} />
         });
 
         return (
